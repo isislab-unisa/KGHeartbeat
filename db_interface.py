839771673a7fbfa3b6f35d2aa15a7c4c959466b0
Gabriele <gabrieletuozzo@gmail.com>
2023-09-25 19:36:41 +0200

    fix: added void file status filed in the db

--- db_interface.py
+++ db_interface.py
@@ -22,13 +22,11 @@ class DBinterface():
 
         #This is done for performance reason, we only store in the most recent analysis all the triples for a specific quality metric, in the old analysis we save only the number 
         #TODO: controllo nel caso SPARQL endpoint e VoID file sono offline, altrimenti inseriremo in modo errato sempre 1 come vecchio valore, se non era disp. nell'ultima analisi, saltiamo la cancellazione del valore. Il controllo è da fare sul vecchio document.
-        #TODO: per fare il controllo sopra è necessario inserire esplicitamente un campo che indichi se VoID file è disponibile (è nel campo extra)
+        #TODO: per fare il controllo sopra è necessario inserire esplicitamente un campo che indichi se VoID file è disponibile (è nel campo extra). Per o vecchi csv (splitted, vedi manual_db_insert)
         if most_recent_document:
             old_uri_regex = utils.to_list(most_recent_document['Representational'][2]['Understandability']['regexUri'])
-            most_recent_document['Representational'][2]['Understandability']['regexUri'] = len(old_uri_regex)
             
             old_serializationFormats = utils.to_list(most_recent_document['Representational'][4]['Versatility']['serializationFormats'])
-            most_recent_document['Representational'][4]['Versatility']['serializationFormats'] = len(old_serializationFormats)
 
             newvalues = { "$set": {
             'Representational.2.Understandability.regexUri': len(old_uri_regex),
@@ -37,19 +35,13 @@ class DBinterface():
 
             self.collection.update_one({'kg_id' : kg_quality.extra.KGid, 'analysis_date' : most_recent_document['analysis_date']}, newvalues)
 
-            '''
-            if isinstance(old_uri_regex,str):
-                old_uri_regex = old_uri_regex.split(';')
-            if isinstance(old_uri_regex,list) and isinstance(kg_quality.understendability.regexUri,list):
-                diff_new_uri_regex = list(set(new_uri_regex) - set(old_uri_regex))
-                kg_quality.understendability.regexUri = diff_new_uri_regex
-            '''
-                
+        availability_dict = kg_quality.availability.__dict__
+        availability_dict['voidAvailability'] = kg_quality.extra.voidAvailability
         obj_to_store = {
             "kg_id" : kg_quality.extra.KGid,
             "kg_name": kg_quality.believability.title,
             "analysis_date" : today_date_str,
-            "Accessibility": [{"Availability" : kg_quality.availability.__dict__,'VoID_availability' : kg_quality.extra.voidAvailability},{"Licensing" : kg_quality.licensing.__dict__}, {"Interlinking" :kg_quality.interlinking.to_dict()}, 
+            "Accessibility": [{"Availability" : availability_dict},{"Licensing" : kg_quality.licensing.__dict__}, {"Interlinking" :kg_quality.interlinking.to_dict()}, 
                               {"Security" : kg_quality.security.__dict__}, {"Performance" : kg_quality.performance.__dict__}],
             "Intrinsic": [{"Accuracy" : kg_quality.accuracy.__dict__}, {"Consistency" : kg_quality.consistency.__dict__}, {"Conciseness" : kg_quality.conciseness.__dict__}],
             "Trust": [{"Reputation" : kg_quality.reputation.to_dict()}, {"Believability" : kg_quality.believability.__dict__}, {"Verifiability" : kg_quality.verifiability.to_dict()}],
