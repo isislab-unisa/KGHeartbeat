e26704c777c7a619fe4f4ebd57db61cb582690df
Gabriele <gabrieletuozzo@gmail.com>
2023-10-07 16:01:43 +0200

    added routes logic

--- front-end/kg-heartbeat/src/App.js
+++ front-end/kg-heartbeat/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import QualityData from './pages/QualityData';
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [selectedKGs, setKG] = useState([]);
+
 
   const handle_search = (results) => {
     setSearchResults(results);
@@ -17,17 +20,27 @@ function App() {
 
   let content;
   if(selectedKGs.length > 0){
-    content = <button>View quality data</button>
+    content = <Link to="/pages/QualityData">Vai a About Us</Link>
   }
 
   console.log(selectedKGs)
   return (
-    <div className="App">
-      <h1>KGHeartbeat</h1>
-      <SearchBar onSearch={handle_search} />
-      {content}
-      <SearchResults results={searchResults} selected={selectedKGs} onCheckboxChange={handleSelectedDataChange} />
-    </div>
+    <Router>
+      <div>
+        <div className="App">
+        <h1>KGHeartbeat</h1>
+        <SearchBar onSearch={handle_search} />
+        {content}
+        <SearchResults results={searchResults} selected={selectedKGs} onCheckboxChange={handleSelectedDataChange} />
+      </div>
+        <Link to="/pages/QualityData" target='_blank'>Vai a About Us</Link>
+        {/* Configura le rotte */}
+        <Routes>
+          <Route path="/pages/QualityData" component={QualityData} element={<QualityData selectedKGs={selectedKGs}/>}/>
+          {/* Altre rotte se necessario */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ front-end/kg-heartbeat/src/pages/QualityData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function QualityData({ selectedKGs }) {
+    console.log(selectedKGs)
+    return (
+      <div>
+        <h1>Display Quality Data</h1>
+      </div>
+    );
+  }
+  
+  export default QualityData;
