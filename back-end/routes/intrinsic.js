950aba131742b5ba72328a28ed3c6cc138fb3b1e
Gabriele <gabrieletuozzo@gmail.com>
2023-10-24 20:23:19 +0000

    added post method for all quality dimensions in the intrinsic category

--- back-end/routes/intrinsic.js
+++ back-end/routes/intrinsic.js
@@ -17,6 +17,21 @@ router.route('/accuracy').get((req,res) =>{
     });
 })
 
+router.route('/accuracy').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 router.route('/consistency').get((req,res) =>{
     const id = req.query.id;
     find_data_over_time(id,quality_category,1).then(result => {
@@ -31,9 +46,39 @@ router.route('/consistency').get((req,res) =>{
     });
 })
 
+router.route('/consistency').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,1).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 router.route('/conciseness').get((req,res) =>{
     const id = req.query.id;
     find_data_over_time(id,quality_category,2).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
+router.route('/conciseness').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,2).then(result => {
         if(result.length > 0)
             res.json(result);
         else
