3954fdaa3b0af679501482e368e9f0d72016c979
Gabriele <gabrieletuozzo@gmail.com>
2023-10-24 17:25:00 +0000

    added post method for security metric

--- back-end/routes/accessibility.js
+++ back-end/routes/accessibility.js
@@ -104,6 +104,21 @@ router.route('/security').get((req, res) =>{
     });
 });
 
+router.route('/security').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_single_data(id_list,quality_category,3).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+});
+
 router.route('/performance').get((req,res) =>{
     const id = req.query.id;
     find_data_over_time(id,quality_category,4).then(result => {
