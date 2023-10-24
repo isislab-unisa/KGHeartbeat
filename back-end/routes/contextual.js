d8cb859885785c27a908212a855b9c22fa6a74cb
Gabriele <gabrieletuozzo@gmail.com>
2023-10-24 17:01:23 +0000

    added post method for amount of data (is useful to get this dimension for multiple KGs)

--- back-end/routes/contextual.js
+++ back-end/routes/contextual.js
@@ -31,5 +31,19 @@ router.route('/amount_of_data').get((req,res) =>{
     });
 })
 
+router.route('/amount_of_data').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
 
 module.exports = router;
