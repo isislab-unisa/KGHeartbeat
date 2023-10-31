7e927d6a1e4a1641af4ca186371e9e5b481e65a3
Gabriele <gabrieletuozzo@gmail.com>
2023-10-31 15:30:00 +0000

    added post methods for all dimensions in the intrinsic category

--- back-end/routes/trust.js
+++ back-end/routes/trust.js
@@ -18,6 +18,21 @@ router.route('/reputation').get((req,res) =>{
     });
 })
 
+router.route('/reputation').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 //TODO: add routes to get only the trust value variations over time ?
 router.route('/believability').get((req, res) =>{
     const id = req.query.id;
@@ -33,6 +48,21 @@ router.route('/believability').get((req, res) =>{
     });
 });
 
+router.route('/believability').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_single_data(id_list,quality_category,1).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 router.route('/verifiability').get((req, res) =>{
     const id = req.query.id;
     find_single_data(id,quality_category,2).then(result => {
@@ -46,6 +76,21 @@ router.route('/verifiability').get((req, res) =>{
         res.status(500).send('Error during the aggregation: ' + err);
     });
 });
+
+router.route('/licensing').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_single_data(id_list,quality_category,2).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
 
 
 module.exports = router;
