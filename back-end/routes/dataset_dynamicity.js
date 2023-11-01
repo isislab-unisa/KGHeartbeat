0a7827101be0cf51ed8f9ba0bab4fdfd420f81df
Gabriele <gabrieletuozzo@gmail.com>
2023-11-01 18:08:36 +0000

    added post methods for dataset dynamicity quality category

--- back-end/routes/dataset_dynamicity.js
+++ back-end/routes/dataset_dynamicity.js
@@ -18,6 +18,21 @@ router.route('/currency').get((req, res) =>{
     });
 });
 
+router.route('/currency').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_single_data(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 router.route('/volatility').get((req, res) =>{
     const id = req.query.id;
     find_single_data(id,quality_category,1).then(result => {
@@ -32,5 +47,19 @@ router.route('/volatility').get((req, res) =>{
     });
 });
 
+router.route('/volatility').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_single_data(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
 
 module.exports = router;
