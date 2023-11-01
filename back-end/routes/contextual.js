9f9073fb16e449039e0f5c63d3feb80b51f5295b
Gabriele <gabrieletuozzo@gmail.com>
2023-11-01 20:36:01 +0000

    added post method for retrieve completeness for multiple KGs by id

--- back-end/routes/contextual.js
+++ back-end/routes/contextual.js
@@ -17,6 +17,21 @@ router.route('/completeness').get((req,res) =>{
     });
 })
 
+router.route('/completeness').post((req,res) => {
+    const body = req.body;   
+    id_list = body.id;
+    find_data_over_time(id_list,quality_category,0).then(result => {
+        if(result.length > 0)
+            res.json(result);
+        else
+            res.status(404).json({ error: 'No data found' });
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Error during the aggregation: ' + err);
+    });
+})
+
 router.route('/amount_of_data').get((req,res) =>{
     const id = req.query.id;
     find_data_over_time(id,quality_category,1).then(result => {
