8b5936133b6b04dfe4223045d2b69cabc6727f3b
Gabriele <gabrieletuozzo@gmail.com>
2023-12-05 08:03:30 +0000

    fix: path when website is hosted in a sub-path

--- front-end/kg-heartbeat/src/App.js
+++ front-end/kg-heartbeat/src/App.js
@@ -30,6 +30,9 @@ import Ranking from './pages/Ranking';
 import Download from './pages/Download';
 import UploadAnalysis from './pages/UploadAnalysis';
 import Homepage from './pages/Homepage';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory({ basename: '/kgheartbeat' });
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
@@ -51,36 +54,36 @@ function App() {
 
   return (
     <div>
-    <Router>
+    <Router basename='/kgheartbeat'>
       <NavBar quality_link={content} score_link={score_link}/>
         <Routes>
-          <Route path="/pages/Homepage" component={Homepage} element={<Homepage/>}/>
-          <Route path="/pages/QualityData" component={QualityData} element={<QualityData selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Search" component={Search} element={<Search searchResults={searchResults} handle_search={handle_search} selectedKGs={selectedKGs} handleSelectedDataChange={handleSelectedDataChange}/>}/>
-          <Route path="/pages/Availability" component={Availability} element={<Availability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Licensing" component={Licensing} element={<Licensing selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Interlinking" component={Interlinking} element={<Interlinking selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Security" component={Security} element={<Security selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Performance" component={Performance} element={<Performance selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Accuracy" component={Accuracy} element={<Accuracy selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Consistency" component={Consistency} element={<Consistency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Conciseness" component={Conciseness} element={<Conciseness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Reputation" component={Reputation} element={<Reputation selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Believability" component={Believability} element={<Believability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Verifiability" component={Verifiability} element={<Verifiability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
-          <Route path="/pages/Currency" component={Currency} element={<Currency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/Volatility" component={Volatility} element={<Volatility selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/Completeness" component={Completeness} element={<Completeness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/AmountOfData" component={AmountOfData} element={<AmountOfData selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/RepresentationalConciseness" component={RepresentationalConciseness} element={<RepresentationalConciseness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/RepresentationalConsistency" component={RepresentationalConsistency} element={<RepresentationalConsistency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/Understandability" component={Understandability} element={<Understandability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
-          <Route path="/pages/Interpretability" component={Interpretability} element={<Interpretability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>                
-          <Route path="/pages/Versatility" component={Versatility} element={<Versatility selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>                
-          <Route path="/pages/Score" component={Score} element={<Score selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>   
-          <Route path='/pages/Ranking' component={Ranking} element={<Ranking />}/>  
-          <Route path='/pages/Download' component={Download} element={<Download />} />           
-          <Route path='/pages/UploadAnalysis' component={UploadAnalysis} element={<UploadAnalysis selectedKGs={selectedKGs} setSelectedKGs={setKG} />} />  
+          <Route basename={'/kgheartbeat'} path="*" component={Homepage} element={<Homepage/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/QualityData" component={QualityData} element={<QualityData selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Search" component={Search} element={<Search searchResults={searchResults} handle_search={handle_search} selectedKGs={selectedKGs} handleSelectedDataChange={handleSelectedDataChange}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Availability" component={Availability} element={<Availability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Licensing" component={Licensing} element={<Licensing selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Interlinking" component={Interlinking} element={<Interlinking selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Security" component={Security} element={<Security selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Performance" component={Performance} element={<Performance selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Accuracy" component={Accuracy} element={<Accuracy selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Consistency" component={Consistency} element={<Consistency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Conciseness" component={Conciseness} element={<Conciseness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Reputation" component={Reputation} element={<Reputation selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Believability" component={Believability} element={<Believability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Verifiability" component={Verifiability} element={<Verifiability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>
+          <Route basename={'/kgheartbeat'} path="/pages/Currency" component={Currency} element={<Currency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/Volatility" component={Volatility} element={<Volatility selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/Completeness" component={Completeness} element={<Completeness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/AmountOfData" component={AmountOfData} element={<AmountOfData selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/RepresentationalConciseness" component={RepresentationalConciseness} element={<RepresentationalConciseness selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/RepresentationalConsistency" component={RepresentationalConsistency} element={<RepresentationalConsistency selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/Understandability" component={Understandability} element={<Understandability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>        
+          <Route basename={'/kgheartbeat'} path="/pages/Interpretability" component={Interpretability} element={<Interpretability selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>                
+          <Route basename={'/kgheartbeat'} path="/pages/Versatility" component={Versatility} element={<Versatility selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>                
+          <Route basename={'/kgheartbeat'} path="/pages/Score" component={Score} element={<Score selectedKGs={selectedKGs} setSelectedKGs={setKG}/>}/>   
+          <Route basename={'/kgheartbeat'} path='/pages/Ranking' component={Ranking} element={<Ranking />}/>  
+          <Route basename={'/kgheartbeat'} path='/pages/Download' component={Download} element={<Download />} />           
+          <Route basename={'/kgheartbeat'} path='/pages/UploadAnalysis' component={UploadAnalysis} element={<UploadAnalysis selectedKGs={selectedKGs} setSelectedKGs={setKG} />} />  
        </Routes>
     </Router>
     </div>
